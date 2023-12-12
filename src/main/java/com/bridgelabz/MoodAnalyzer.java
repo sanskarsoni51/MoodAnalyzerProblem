ad4b130e93adb236b66e5df44d84bb5f09859b8f
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-12-12 12:11:34 +0530

    Added: Functionality of analyzing the message and respond accordingly

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+    String AnalyzeMood(String Message){
 
+        if (Message.contains("I am in sad mood")){
+            return "SAD!";
+        } else if (Message.contains("I am in happy mood")) {
+            return "HAPPY!";
+        }
+
+        return null;
+    }
 
 }
--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,5 +1,24 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+    MoodAnalyzer Analyze = new MoodAnalyzer();
+    Assert Assertions = null;
+
+    @Test
+    public void MoodIsSad(){
+        String mood = Analyze.AnalyzeMood("I am in sad mood");
+
+        Assertions.assertEquals("SAD!",mood);
+    }
+
+    @Test
+    public void MoodIsHappy(){
+        String mood = Analyze.AnalyzeMood("I am in happy mood");
+
+        Assertions.assertEquals("HAPPY!",mood);
+    }
 
 }
